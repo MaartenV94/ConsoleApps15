e279722ee23867682afdc17c6127f4ca720e8d12
Maarten Vanderbeeken <maartenvanderbeeken@hotmail.com>
2022-02-06 10:31:10 +0000

    Added empty methods and attributes

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,13 +1,42 @@
 ﻿namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This app lets a user enter a unit of distance in miles
+    /// Then is calculated into feet and outputs the result
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Maarten Vanderbeeken version 1.0
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
 
+        private double feet;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Run()
+        {
+
+        }
+        /// <summary>
+        /// Promt the user to enter the distance in miles
+        /// Input the miles as a double number
+        /// </summary>
+        private void InputMiles()
+        {
+
+        }
+
+        private void CalculateFeet()
+        {
+
+        }
+
+        private void OutputFeet()
+        {
+
+        }
     }
 }
