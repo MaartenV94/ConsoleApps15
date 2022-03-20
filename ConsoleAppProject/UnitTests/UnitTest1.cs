7db864422763cd7c247ab87a2019033f7ca60d32
maartenvanderbeeken <maartenvanderbeeken@hotmail.com>
2022-03-20 14:26:03 +0000

    App03 Unit tests

---
+++ ConsoleAppProject/UnitTests/UnitTest1.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleAppProject.App03;
+
+namespace ConsoleApp.Tests
+{
+    [TestClass]
+    public class App03Test
+    {
+        private StudentGrades
+            converter = new StudentGrades();
+
+        private readonly int[] StatsMarks = new int[]
+        {
+            10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+        };
+
+        [TestMethod]
+        public void TestConvert0ToGradeF()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.F;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrades(0);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert39ToGradeF()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.F;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrades(39);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert40ToGradeD()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.D;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrades(40);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert49ToGradeD()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.D;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrades(49);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert50ToGradeC()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.C;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrades(50);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert59ToGradeC()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.C;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrades(59);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert60ToGradeB()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.B;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrades(60);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert69ToGradeB()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.B;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrades(69);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert70ToGradeA()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.A;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrades(70);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestConvert99ToGradeA()
+        {
+            //Arrange
+            Grades expectedGrade = Grades.A;
+
+            //Act
+            Grades actualGrade = converter.ConvertToGrades(99);
+
+            //Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+
+        [TestMethod]
+        public void TestCalculateMin()
+        {
+            //Arrange
+            converter.Marks = StatsMarks;
+            int expectedMin = 10;
+
+            //Act
+            converter.CalculateStats();
+
+            //Assert
+            Assert.AreEqual(expectedMin, converter.Minimum);
+        }
+
+        [TestMethod]
+        public void TestGradeProfile()
+        {
+            //Arrange
+            converter.Marks = StatsMarks;
+
+            //Act
+            converter.CalculateGradeProfile();
+
+            bool expectedProfile;
+            expectedProfile = ((converter.GradeProfile[0] == 3) &&
+                               (converter.GradeProfile[1] == 1) &&
+                               (converter.GradeProfile[2] == 1) &&
+                               (converter.GradeProfile[3] == 1) &&
+                               (converter.GradeProfile[4] == 4));
+            //Assert
+            Assert.IsTrue(expectedProfile);
+        }
+    }
+}
